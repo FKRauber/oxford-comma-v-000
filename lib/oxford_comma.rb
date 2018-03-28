960da2855aebd90a985b2e99390c1aa1cb7dc86b
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-03-28 16:21:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,14 +4,17 @@ def oxford_comma(array)
       return i
     end
   elsif array.length == 2
+    i_array=[]
     array.each do |i|
-      i.join(" and ")
+      i_array.push(i.join(" and "))
     end
-    return i
+    return i_array
   elsif array.length == 3
-    array.each do |i|
-      i.join(",")
+    x_array=[]
+    array.each do |x|
+      x_array.push(x.join(","))
     end
+    return x_array
   else
     array.split
   end
