f79c6b62031e33a7fbf6a56f33f1a888b75130a8
Fabienne Rauber <fabienne.rauber@gmail.com>
2018-03-28 17:05:43 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,11 @@
 def oxford_comma(array)
   if array.length == 1
-    array.each do |i|
       return i
-    end
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
     array.join(", ")
+    array.last
   else
     array.split
     return array
